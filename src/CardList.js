a344a40018a43b2a952ef8438d7ae846b9c7a168
Furkan Korkmaz <hfkorkmaz1@gmail.com>
2020-11-08 22:20:18 +0300

    removed console log

--- src/CardList.js
+++ src/CardList.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Card from "./Card";
 export default function CardList({ questions }) {
-    console.log(questions)
   return (
     <div className="card-grid">
-      {questions.map((question) => {
-        return <Card question={question} key={question.question}></Card>;
-      })}
-
+      {questions.map((question) => (
+        <Card question={question} key={question.question}></Card>
+      ))}
     </div>
   );
 }
